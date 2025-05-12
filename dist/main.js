8fed0b4ef3e2a1bb6690b44390267699ad10174c
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-05-11 22:18:53 -0600

    Agregando las reglas y modificando el responsive

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("Hola mundo");
+const botonReglas = document.getElementById('reglas__boton');
+const paginaReglas = document.getElementById('pagina__reglas__id');
+const cerrarReglas = document.getElementById('cerrar__boton');
+botonReglas.addEventListener('click', () => {
+    paginaReglas.classList.toggle('active');
+});
+cerrarReglas.addEventListener('click', () => {
+    paginaReglas.classList.toggle('hidden');
+});
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
-console.log("Hola mundo");
+const botonReglas = document.getElementById('reglas__boton') as HTMLButtonElement;
+const paginaReglas = document.getElementById('pagina__reglas__id') as HTMLDivElement;
+const cerrarReglas = document.getElementById('cerrar__boton') as HTMLButtonElement;
 
+botonReglas.addEventListener('click', () => {
 
+    paginaReglas.classList.toggle('active');
 
+});
+
+cerrarReglas.addEventListener('click', () => {
+
+        paginaReglas.classList.toggle('hidden');
+        
+});
 
