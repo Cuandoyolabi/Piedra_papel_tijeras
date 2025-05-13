c78c582cd91ee48d312d405aa2d3bb9c9532479d
Adam Ramirez <130320680+Cuandoyolabi@users.noreply.github.com>
2025-05-13 10:18:50 -0600

    Se implemento el codigo correctamente, esta casi lista la seccion de reglas

--- dist/main.js
+++ dist/main.js
@@ -1,10 +1,13 @@
 "use strict";
 const botonReglas = document.getElementById('reglas__boton');
 const paginaReglas = document.getElementById('pagina__reglas__id');
+const modal__oscuro = document.getElementById('modal__oscuro__id');
 const cerrarReglas = document.getElementById('cerrar__boton');
 botonReglas.addEventListener('click', () => {
+    modal__oscuro.classList.add('active');
     paginaReglas.classList.add('active');
 });
 cerrarReglas.addEventListener('click', () => {
+    modal__oscuro.classList.remove('active');
     paginaReglas.classList.remove('active');
 });
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,18 @@
 const botonReglas = document.getElementById('reglas__boton') as HTMLButtonElement;
 const paginaReglas = document.getElementById('pagina__reglas__id') as HTMLDivElement;
+const modal__oscuro = document.getElementById('modal__oscuro__id') as HTMLDivElement;
 const cerrarReglas = document.getElementById('cerrar__boton') as HTMLButtonElement;
 
 botonReglas.addEventListener('click', () => {
 
+    modal__oscuro.classList.add('active');
     paginaReglas.classList.add('active');
 
 });
 
 cerrarReglas.addEventListener('click', () => {
 
+    modal__oscuro.classList.remove('active');
     paginaReglas.classList.remove('active');
 
 });
